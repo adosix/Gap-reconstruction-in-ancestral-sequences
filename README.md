135f6d44bbe801cf73f98456f37bbbdfecb10d7d
adosix <andrejjezik@gmail.com>
2022-04-09 17:45:50 +0200

    final refactor and storing output sequences in separate .fasta files

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Gap-reconstruction-in-ancestral-sequences
+
+requires one argument, specifying the file with MSA.
+
+## Example of running the script
+
+python reconstruct.py given_inputs/msa.fasta
--- reconstruct.py
+++ reconstruct.py
@@ -2,12 +2,13 @@
 import sys
 from Bio import Phylo
 import csv
+import os
 
 DEBUG = False
 DEBUG_LONG = False
-
-ANCESTRAL_FILE = "ancestrals.csv"
-PHYLO_TREE_FILE = "tree.tre"
+ANCESTRAL_FILE = "given_inputs/ancestrals.csv"
+PHYLO_TREE_FILE = "given_inputs/tree.tre"
+OUTPUT_DIR = "output_sequences"
 
 # Read a FASTA file as list of pairs [(sequence id, sequence), ..]
 
@@ -42,51 +43,50 @@ def take(dct, low=None):
     return dict(list(dct.items())[low:])
 
 
+def insert_newlines(string, every):
+    return '\n'.join(string[i:i+every] for i in range(0, len(string), every))
+
+
 # --------------- Main -------------------
 if __name__ == '__main__':
     if (len(sys.argv) != 2):  # checking arguments
         print("ERROR: program expects one argument with path of a input file\n", file=sys.stderr)
-
     list_of_fasta_seq = read_fasta(file_name=sys.argv[1])
-
     tree = Phylo.read(PHYLO_TREE_FILE, "newick")
-
-    # tree.ladderize()
-    # Phylo.draw(tree)
-
     ancestrals_list = read_csv(ANCESTRAL_FILE)
 
-    terminals = tree.get_terminals()
-    # print(terminals)
+    if not os.path.exists(OUTPUT_DIR):
+        os.makedirs(OUTPUT_DIR)
+
     for clade in tree.get_nonterminals():
         rced_seq = [""] * len(list_of_fasta_seq[0][1])
-        # sum(node sequences which has gap on i position, distance betwee them)
+        # couner for each position of sequence which determines if on the position will be gap
         gap = [0] * len(list_of_fasta_seq[0][1])
-        # sum(node sequences which has no gap on i position, distance betwee them)
-        not_gap = [0] * len(list_of_fasta_seq[0][1])
-        for t in terminals:
+        for t in tree.get_terminals():
             # iterates throught each position of sequence we are constructing
             for i in range(len(list_of_fasta_seq[0][1])):
-
                 if(clade.is_parent_of(t)):
                     for sublist in list_of_fasta_seq:
                         if sublist[0] == str(t):
                             if(sublist[1][i] == "-"):
                                 gap[i] += clade.distance(str(t))
                             else:
-                                not_gap[i] += clade.distance(str(t))
+                                gap[i] -= clade.distance(str(t))
                             break
 
         nodes = [x for x in ancestrals_list if x['node']
                  == str(clade.confidence)]
+        # creates sequence
         for i in range(len(list_of_fasta_seq[0][1])):
-            if(gap[i] > not_gap[i]):
+            if(gap[i] > 0):
                 rced_seq[i] = '-'
             else:
                 rced_seq[i] = max(take(nodes[i], 2), key=nodes[i].get)
         rced_seq_str = "".join(map(str, rced_seq))
+
         print(str(clade.confidence) + " --> " + rced_seq_str)
-        # print(len(list_of_fasta_seq[0][1]))
-        # trace
-        # distance
-        # is_parent_of
+
+        f = open(OUTPUT_DIR + "/node_" +
+                 str(clade.confidence) + ".fasta", "w+")
+        f.write(">" + str(clade.confidence) + "\n" +
+                insert_newlines(rced_seq_str, 60))
